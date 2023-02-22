0fe75e631baeb7b395c259a684e4447593874b31
josephCorella <100166928+josephCorella@users.noreply.github.com>
2023-02-22 01:53:40 -0600

    modulos home user residencia

--- client/appAngular/src/app/app.module.ts
+++ client/appAngular/src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { ShareModule } from './share/share.module';
+import { HomeModule } from './home/home.module';
+import { UserModule } from './user/user.module';
+import { ResidenciaModule } from './residencia/residencia.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ AppComponent  ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    
+    BrowserAnimationsModule,
+    CoreModule,
+    ShareModule,
+    HomeModule,
+    UserModule,
+    ResidenciaModule,
+    
     AppRoutingModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/appAngular/src/app/residencia/residencia.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ResidenciaRoutingModule } from './residencia-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ResidenciaRoutingModule
+  ]
+})
+export class ResidenciaModule { }
