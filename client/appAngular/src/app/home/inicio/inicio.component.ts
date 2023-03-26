e902154c07b5af02f0b70946a4e17c7f715aacb9
josephCorella <100166928+josephCorella@users.noreply.github.com>
2023-03-26 01:01:44 -0600

    informacion en inicio

--- client/appAngular/src/app/home/inicio/inicio.component.ts
+++ client/appAngular/src/app/home/inicio/inicio.component.ts
@@ -1,4 +1,9 @@
 import { Component , OnInit} from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { GenericService } from 'src/app/share/generic.service';
+import { NotificacionService, TipoMessage } from 'src/app/share/notification.service';
 
 @Component({
   selector: 'app-inicio',
@@ -6,10 +11,40 @@ import { Component , OnInit} from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  datos:any;
+  destroy$: Subject <boolean>= new Subject<boolean>();
 
-  constructor() { }
+  constructor(private gService:GenericService,
+    private notificacion:NotificacionService,
+    private router:Router,
+    private route:ActivatedRoute,
+    private dialog:MatDialog
+    ) { 
+    this.listaInformacion();
+  }
 
   ngOnInit(): void {
   }
+
+  listaInformacion(){
+    //Llamar al API, nombre de ruta
+    this.gService.list('informacion/')
+    .pipe(takeUntil(this.destroy$))
+    .subscribe((data:any)=>{
+      console.log(data);
+      this.datos=data;
+    });
+  }
+
+
+  ngOnDestroy(){
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+
+
+
+
 
 }
